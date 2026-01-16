1b07409817281fc5bd12fc7f13eb4fb2e9fef754
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2026-01-16 13:33:50 +0800

    Requesting for camera permission.

---
+++ camera-scanning/camera-scanning/camera_scanningApp.swift
@@ -0,0 +1,17 @@
+//
+//  camera_scanningApp.swift
+//  camera-scanning
+//
+//  Created by Yan Cheng Cheok on 16/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct camera_scanningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
